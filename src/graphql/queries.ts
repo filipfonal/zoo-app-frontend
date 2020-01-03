58fc4a938ca656274a305f3c22333b04570d8137
Filip Fonal <filipfonal@gmail.com>
2020-01-03 21:14:18 +0100

    Create graphql queries file

---
+++ src/graphql/queries.ts
@@ -0,0 +1,62 @@
+import gql from 'graphql-tag';
+
+export const FIND_CITIES_QUERY = gql`
+    query findCities($query: String!) {
+        findCities(query: $query){
+            name,
+            fullName
+            location {
+                longitude
+                latitude
+            }
+        }
+    }
+`;
+
+export const FIND_NEARBY_ZOOS = gql`
+    query findNearbyZoos($longitude: Float!, $latitude: Float!, $range: Int!) {
+        findNearbyZoos(longitude: $longitude, latitude: $latitude, range: $range){
+            id
+            name
+            location {
+                longitude
+                latitude
+            }
+            description
+            logo
+            wikipediaLink
+            address {
+                full
+                city
+                state
+                country
+                countryCode
+            }
+            myReview {
+                id
+                user {
+                    id
+                    name
+                    email
+                }
+                zooId
+                rating
+                content
+                createdAt
+            }
+            recentReviews {
+                id
+                user {
+                    id
+                    name
+                    email
+                }
+                zooId
+                rating
+                content
+                createdAt
+            }
+            rating
+        }
+    }
+`;
